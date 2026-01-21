67238a22c7f49bf7661e6ead0e3b3863211b2780
campero9813 <campero9813@gmail.com>
2026-01-20 20:46:01 -0600

    Actualizacion url prueba2

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from app.models.simulation_model import Simulation
 app = FastAPI(title="Tabla de Amortizacion Microservicio")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://amortization-front-prueba.vercel.app", "http://localhost:5173"],
+    allow_origins=["https://amortization-front-prueba.vercel.app", "http://localhost:5173", "https://amortization-front-prueba-q2xfjc2a9-alejandro-camperos-projects.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
