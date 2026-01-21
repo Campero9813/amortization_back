5485b3cff9581481fc70e88d499c604596a702fc
campero9813 <campero9813@gmail.com>
2026-01-20 20:51:44 -0600

    Actualizacion de CORS para solicitudes y pruebas

--- app/main.py
+++ app/main.py
@@ -10,7 +10,12 @@ from app.models.simulation_model import Simulation
 app = FastAPI(title="Tabla de Amortizacion Microservicio")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://amortization-front-prueba.vercel.app", "http://localhost:5173", "https://amortization-front-prueba-q2xfjc2a9-alejandro-camperos-projects.vercel.app/"],
+    allow_origins=[
+        "https://amortization-front-prueba.vercel.app", 
+        "http://localhost:5173",
+        "http://192.168.4.32", 
+        "http://192.168.4.254",
+        "https://amortization-front-prueba-q2xfjc2a9-alejandro-camperos-projects.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
